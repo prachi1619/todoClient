f63a9fc11384afee10443de7b9a8ccda5b43ae6e
Prachi Gupta <priyaprachi905@gmail.com>
2024-03-18 02:18:21 +0530

    Backend URL Fix

--- src/services/auth.api.js
+++ src/services/auth.api.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const API_URL = "http://localhost:9000/api/v1";
+const API_URL = "https://todoserver-s269.onrender.com/api/v1";
 
 export const login = async (user) => {
   try {
